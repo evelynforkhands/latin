55459df82bbf7b844cd82658f626279fb9e03517
evelynforkhands <lubaevadaria@gmail.com>
2018-12-25 14:25:53 +0300

    removed else from lastUpdate

--- latin-bot.py
+++ latin-bot.py
@@ -26,8 +26,8 @@ class BotHandler:
         
         if len(get_result) > 0:
             last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
+        #else:
+         #   last_update = get_result[len(get_result)]
 
         return last_update
 
